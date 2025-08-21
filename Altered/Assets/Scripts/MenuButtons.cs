24148cca09beb5f5f4132befb2696c8bbb83b460
Colin <colinbeijer1@gmail.com>
2025-08-21 21:43:59 +0200

    Added basic funtions to menu

--- Altered/Assets/Scripts/MenuButtons.cs
+++ Altered/Assets/Scripts/MenuButtons.cs
@@ -6,15 +6,28 @@ using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
+    // Open a new menu panel (e.g., for sub-menus)
     public void OpenNewMenu(GameObject newMenu)
     {
         newMenu.SetActive(true);
         UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.transform.parent.gameObject.SetActive(false);
     }
-    
-    // To make a button change scenes.
-    public void ChangeScene()
+
+    // Load the DeckManagerScene
+    public void ChangeSceneToDeckManager()
     {
-        
+        SceneManager.LoadScene("DeckManagerScene");
+    }
+
+    // Load the PlayMenuScene
+    public void ChangeSceneToPlayMenu()
+    {
+        SceneManager.LoadScene("PlayMenuScene");
+    }
+
+    // Return to the MainMenuScene
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
     }
 }
